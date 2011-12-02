dc162cfbe65ee30ac00dd69fcfd7c5b1ac2987d0
Shomari Kendrick <gyf803@my.utsa.edu>
2011-12-02 15:41:18 -0600

    Editing layout_links

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -4,22 +4,22 @@ describe "LayoutLinks" do
   
     it "should have a home page at '/'" do
       get '/'
-      response.should have_selector("title", :content => "Kays App | Home")
+      response.should have_selector("title", :content => "Home")
     end
   
      it "should have a contact page at '/contact'" do
       get '/contact'
-      response.should have_selector("title", :content => "Kays App | Contact")
+      response.should have_selector("title", :content => "Contact")
     end
     
      it "should have an about page at '/about'" do
       get '/about'
-      response.should have_selector("title", :content => "Kays App | About")
+      response.should have_selector("title", :content => "About")
     end
     
     it "should have a help page at '/help'" do
       get '/help'
-      response.should have_selector("title", :content => "Kays App | Help")
+      response.should have_selector("title", :content => "Help")
     end
     
      it "should have a sign-up page at '/signup'" do
