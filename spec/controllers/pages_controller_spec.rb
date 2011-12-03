ccf42c40354f2d9670141284c6a11d9af4a9b360
Shomari Kendrick <gyf803@my.utsa.edu>
2011-12-02 22:27:40 -0600

    Edited spec page controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,12 +6,12 @@ describe PagesController do
 
   describe "GET 'home'" do
     it "returns http success" do
-      get 'home'
+      get :home
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'home'
+      get :home
       response.should have_selector("title", :content => "Home")
     end
 
