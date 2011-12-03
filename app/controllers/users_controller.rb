589619d8ed6f5380929a52a78a17f601f01a8861
Shomari Kendrick <gyf803@my.utsa.edu>
2011-12-02 21:15:35 -0600

    Edited users controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def new # responds to get /users/new by rendering new.html.erb
     @title = "Sign up"
-    @user = User.find(params[:id])
+    @user = User.new
   end
   
   def create # responds to post /users and redirects to /user/i if successful or new.html.erb if not
