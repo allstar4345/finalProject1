cf8096d54062a750003806154de7cb7932caefc4
Shomari Kendrick <gyf803@my.utsa.edu>
2011-12-11 21:11:22 -0600

    Sessions helper

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ FinalProject::Application.routes.draw do
   resources :sessions, :only => [:new, :create, :destroy]
   resources :microposts, :only => [:create, :destroy]
   
+  
+  root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/signup', :to => 'users#new'
   match '/signin', :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
-  root :to => 'pages#home'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
