3d8ffac87b5c83c2ffd76ae96b4e6efb99373f8f
Shomari Kendrick <gyf803@my.utsa.edu>
2011-12-12 03:17:58 -0600

    Adding helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,16 @@
 module ApplicationHelper
   
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Family Connect"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
   def logo
-    image_tag("tree.gif", :alt => "Final Project", :class => "round")
+    image_tag("tree.gif", :alt => "Family Connect", :class => "round")
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,21 @@
 module SessionsHelper
   
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.id
+    cookies.permanent[:remember_token] = [user.email, user.password]
     current_user = user
+  end
+  
+  def sign_out
+    cookies.delete(:remember_token)
+    current_user= nil
   end
   
   def current_user=(user)
     @current_user = user
+  end
+  
+  def current_user?(user)
+    user == current_user
   end
   
   def current_user
@@ -16,14 +25,24 @@ module SessionsHelper
   def signed_in?
       !(current_user.nil?)
   end
+  
+  def authenticate
+     deny_access unless signed_in?
+  end
+  
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page"
+  end
 
   private
     def user_from_remember_token
-      userId = remember_token
-      User.find_by_id(userId)
+      userEmail = remember_token
+      User.find_by_email(userEmail)
     end
   
     def remember_token
       cookies[:remember_token] || nil
     end
+  
 end
