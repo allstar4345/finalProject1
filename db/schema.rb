61635058f465e5d0da4da924684f29578f0fbf87
Shomari Kendrick <gyf803@my.utsa.edu>
2011-12-12 06:37:49 -0600

    DB edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,30 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100831012055) do
+ActiveRecord::Schema.define(:version => 20111212113403) do
 
-  create_table "microposts", :force => true do |t|
-    t.string "content"
-    t.integer "user_id"
+  create_table "groups", :force => true do |t|
+    t.integer  "follower_id"
+    t.integer  "leader_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
+  add_index "groups", ["follower_id"], :name => "index_groups_on_follower_id"
+  add_index "groups", ["leader_id"], :name => "index_groups_on_leader_id"
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "microposts", ["user_id"], :name => "index_microposts_on_user_id"
 
   create_table "relationships", :force => true do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+    t.integer  "follower_id"
+    t.integer  "followed_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -34,15 +44,14 @@ ActiveRecord::Schema.define(:version => 20100831012055) do
   add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
 
   create_table "users", :force => true do |t|
-    t.string "name"
-    t.string "email"
+    t.text     "password"
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "encrypted_password"
-    t.string "salt"
-    t.boolean "admin", :default => false
+    t.string   "encrypted_password"
+    t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
