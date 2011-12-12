89743e9597fd69a41a77a262de4b40f944f1e288
Shomari Kendrick <gyf803@my.utsa.edu>
2011-12-12 04:32:16 -0600

    edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,4 +53,10 @@ class User < ActiveRecord::Base
     groups.find_by_leader_id(leader).destroy
   end
   
+  class << self
+    def authenticate(email, submitted_password)
+      user = find_by_email(email)
+      (user && user.has_password?(submitted_password)) ? user : nil
+    end
+  end
 end
