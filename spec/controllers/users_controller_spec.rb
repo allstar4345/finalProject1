95de2d17a4337bd40707a3d8a20a96b923fd6d81
Shomari Kendrick <gyf803@my.utsa.edu>
2011-12-12 07:40:09 -0600

    edit user cont spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -195,7 +195,7 @@ describe UsersController do
 
       it "should have a welcome message" do
         post :create, :user => @attr
-        flash[:success].should =~ /welcome to the sample app/i
+        flash[:success].should =~ /welcome to family connect/i
       end
       
       it "should sign the user in" do
